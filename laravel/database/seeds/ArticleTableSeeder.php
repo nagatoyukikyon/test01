aad435b2be5854254bade6cefdfe6713f60f1f3b
Ahlang <root@localhost.localdomain>
2016-08-25 15:30:43 +0800

    Add test.rb to test git function

---
+++ laravel/database/seeds/ArticleTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Article ;
+class ArticleTableSeeder extends Seeder
+{
+   
+    public function run()
+    {
+        DB::table('articles')->truncate;
+        for($i=0; $i<10 ;$i++){
+            Article::create([
+                'title'=>str_random(10),
+                'content'=>str_random(255)
+
+            ]);
+        }
+    }
+
+}
